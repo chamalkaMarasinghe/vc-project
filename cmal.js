6d2aa0a308de7d24521509c01ddea7ea767a40d3
chamalkaMarasinghe <108983037+chamalkaMarasinghe@users.noreply.github.com>
2024-02-18 01:55:54 +0530

    Update cmal.js

--- cmal.js
+++ cmal.js
@@ -1,6 +1,1 @@
-console.log("change 1 from chamalkaMarasinghe local")
-
-console.log("change - 1 from chamalka2000")
-console.log("change - 2 deom chmalka marasinghe")
-
-console.log("change - 3 from cmal553 local")
+console.log("totally different")
