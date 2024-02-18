bbaa32d0d93c168f57279ae66aee7edace4fb17a
chamalkaMarasinghe <108983037+chamalkaMarasinghe@users.noreply.github.com>
2024-02-18 21:11:55 +0530

    Update chamalka.js

--- chamalka.js
+++ chamalka.js
@@ -1,7 +1,1 @@
-
-console.log("change 2")
-console.log("change 3")
-console.log("change 5")
-console.log("change 7")
-console.log("change 8")
-console.log("change 9")
+console.log('pull req conflict 1')
